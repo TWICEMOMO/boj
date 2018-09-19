4c6fdef73728268aec703497dfb83915566d494a
TWICEMOMO <rlaehdwn3331@gmail.com>
2018-09-19 19:55:51 +0900

    graph

--- graph/11404.cc
+++ graph/11404.cc
@@ -1,34 +1,43 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+#include <functional>
+#include <utility>
+#include <cstdio>
 using namespace std;
 
-const int INF = 1000000007;
-int n, m;
-int a[101][101];
+typedef pair<int, int> P;
+const int MAX = 101;
+const int INF = 200000;
+int n, dist[MAX][MAX], k;
 int main() {
-	cin >> n >> m;
+	cin >> n>>k;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if (i != j) a[i][j] = INF;
+			dist[i][j] = i == j ? 0 : INF;
 		}
-	}
-	for (int i = 0; i < m; i++) {
-		int y, x, w; cin >> y >> x >> w;
-		if (a[y - 1][x - 1] > w) a[y - 1][x - 1] = w;
+    }
+
+	for (int i = 0; i < k; i++) {
+		int a, b, c;
+		scanf("%d %d %d", &a, &b, &c);
+		dist[a - 1][b - 1] = min(dist[a-1][b-1],c);
 	}
 
-	for (int k = 0; k < n; k++) {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				a[i][j] = min(a[i][j], a[i][k] + a[k][j]);
-			}
-		}
-	}
+	for (int k = 0; k < n; k++)
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			cout << a[i][j] << " ";
+			if (dist[i][j] == INF || dist[i][j]==0)
+				printf("0 ");
+			else
+			    printf("%d ", dist[i][j]);
 		}
-		cout << '\n';
+		cout << endl;
 	}
-	return 0;
+    return 0;
 }
